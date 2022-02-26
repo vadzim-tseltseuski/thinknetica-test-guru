b95befd34f75962a037cf04ec60836e586a7e037
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-02-26 21:59:36 +0300

    feat: add show action to question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,23 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   def index
     render inline: "<ol><% @test.questions.each do |p| %><li><%= p.body %></li><% end %></ol>"
+  end
+
+  def show
+    render inline: "<%= @question.body %>"
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
 end
