0162c95a9f54e1ba650fc972d714bc4d93a7a4fb
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-02-26 21:53:11 +0300

    feat: add index action to question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+
+  def index
+    render inline: "<ol><% @test.questions.each do |p| %><li><%= p.body %></li><% end %></ol>"
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
 end
