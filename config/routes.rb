8963e3b1f88b7c29541dcc20091d6b125646e921
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-04-20 01:18:16 +0400

    feat: add answer controller, routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
+
+
 end
