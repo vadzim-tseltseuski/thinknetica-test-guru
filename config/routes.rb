f3c152dca49b3b753e92cb357ca3512bf0387348
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-04-19 22:48:25 +0400

    fix: PR after review

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index
   end
 end
