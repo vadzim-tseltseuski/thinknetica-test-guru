3055863bdc0bbd34a6014c0140e751df1a9e5f91
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-05-17 19:46:47 +0400

    feat: add mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: %{"TestGuru" <admin@testguru.com>}
   layout 'mailer'
 end
