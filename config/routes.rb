25b76d66170570df65baacec6da62967e2126335
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-05-17 22:50:21 +0400

    feat: add to registration form first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
+                            controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :tests, only: :index do
     member do
