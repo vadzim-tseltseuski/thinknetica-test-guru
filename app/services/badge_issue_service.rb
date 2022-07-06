7796761d6cecdc6d78fc4afa6716c6fdd8871418
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-07-07 00:14:02 +0400

    fix

--- app/services/badge_issue_service.rb
+++ app/services/badge_issue_service.rb
@@ -35,7 +35,7 @@ class BadgeIssueService
       return false if category_id != @test.category_id
 
       all_category_test_ids = Test.where(category_id: category_id).ids.sort
-      completed_category_test_ids = @user.tests.by_category_name(category_title).ids.sort
+      completed_category_test_ids = @user.tests.where(category_id: category_id).ids.sort
 
       completed_category_test_ids == all_category_test_ids
     end
