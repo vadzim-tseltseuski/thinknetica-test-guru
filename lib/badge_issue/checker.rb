7908120f99706237c3d6e288dc221ff11b3ff0ba
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-07-07 14:20:14 +0400

    fix

--- lib/badge_issue/checker.rb
+++ lib/badge_issue/checker.rb
@@ -1,18 +1,22 @@
 class BadgeIssue::Checker
   def self.satisfied_badges(test_passage)
-    new.satisfied_badges(test_passage)
+    new(test_passage).satisfied_badges
   end
 
-  def satisfied_badges(test_passage)
-    @badges = Badge.select do |badge|
-      is_satisfied?(badge)
-    end
+  def initialize(test_passage)
+    @test_passage = test_passage
   end
 
   private
 
+  def satisfied_badges
+    Badge.select do |badge|
+      is_satisfied?(badge)
+    end
+  end
+
   def is_satisfied?(badge)
     rule_class = "badge_issue/rule/#{badge.rule.downcase}".camelize.constantize
-    rule_class.new(test_passage).is_satisfied_by?(badge.rule_option)
+    rule_class.new(@test_passage).is_satisfied_by?(badge.rule_option)
   end
 end
